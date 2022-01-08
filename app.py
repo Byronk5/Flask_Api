82c89c3fad488fe9f86ab2a47ec7aef33524370f
Byron <byronk5@gmail.com>
2022-01-08 21:23:13 +0200

    Profile Routes Created-CRUD

--- app.py
+++ app.py
@@ -107,11 +107,120 @@ def login():
     if check_password_hash(pw, auth.get('password')):
 
         token = jwt.encode({"id": str(user.id), 'exp': datetime.datetime.utcnow(
-        ) + datetime.timedelta(seconds=300)}, app.config['SECRET_KEY'], algorithm="HS256")
+        ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
 
         return make_response(jsonify({'token': token}), 201)
 
     return make_response('could not verify', 403, {'WWW-Authenticate': 'Basic realm ="Wrong Password !!"'})
+
+
+# PROFILE ROUTES
+
+# Profile POST
+
+
+@app.route('/profile/', methods=['POST'])
+@token_required
+def create_profile(current_user):
+    try:
+        data = request.get_json()
+
+        user_profile = Profile(
+            id_user=str(current_user.id), name=data['name'], surname=data['surname'], phone=data['phone'])
+
+        user_profile.save()
+
+        return jsonify({"Message": "User Profile Created!"}), 201
+
+    except Exception as ex:
+        jsonify(ex)
+        return jsonify({"Message": "Unable to create User!"}), 401
+
+
+# Profile GET - List Profiles
+@app.route('/list-profiles/', methods=["GET"])
+@token_required
+def list_profiles(current_user):
+    try:
+        output = []
+
+        for profile in Profile.objects():
+
+            profileData = {}
+            profileData['id_user'] = profile.id_user
+            profileData['name'] = profile.name
+            profileData['surname'] = profile.surname
+            profileData['created'] = profile.created
+            profileData['updated'] = profile.updated
+
+        output.append(profileData)
+
+        return jsonify(output), 200
+
+    except Exception as ex:
+        print(ex)
+        return jsonify({"Message": "Unable to Retrieve Profiles"}), 404
+
+ # Profile GET - Single Profile
+
+
+@app.route('/profile/<user_id>', methods=["GET"])
+@token_required
+def get_single_profile(current_user, user_id):
+    try:
+        user_profile = Profile.objects(id=user_id)
+
+        if not user_profile:
+            return jsonify({"Message": "User not found!"}), 404
+
+        return jsonify({"User Profile": user_profile}), 200
+
+    except Exception as ex:
+        print(ex)
+
+
+# Profile PUT
+
+# Updates the updated field in both the user and profile collections
+# id_user is passed instead of the Mongodb Object ID since the user and the user profile share this id
+
+
+@app.route('/profile/<user_id>', methods=["PUT"])
+@token_required
+def update_user_profile(current_user, user_id):
+    try:
+        user_profile = Profile.objects(id_user=user_id).get()
+        user = User.objects(id=user_id).get()
+
+        if not user_profile and not user:
+            return jsonify({"Message": "User Profile not found!"}), 404
+
+        user_profile.updated = datetime.datetime.now()
+        user.updated = datetime.datetime.now()
+        user_profile.save()
+        user.save()
+
+        return jsonify({"Message": "User Profile Updated"}), 201
+    except Exception as ex:
+        print(ex)
+
+
+# Profile DELETE
+
+@app.route('/profile/<user_id>', methods=["DELETE"])
+@token_required
+def delete_profile(current_user, user_id):
+    try:
+        user_profile = Profile.objects(id=user_id)
+
+        if not user_profile:
+            return jsonify({"Message": "User Profile not found!"}), 404
+
+        user_profile.delete()
+        return jsonify({"Message": "User Profile deleted!"}), 200
+
+    except Exception as ex:
+        print(ex)
 
 
 if __name__ == "__main__":

8f96743744f1779dc8bf640c94a3e2cfda26b5a5
Byron <byronk5@gmail.com>
2022-01-08 16:13:16 +0200

    Models & Register Route Created

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
 from flask_mongoengine import MongoEngine
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -13,10 +16,44 @@ db.init_app(app)
 
 app.config['SECRET_KEY'] = 'secret'
 
+# Models
 
-@app.route('/test/', methods=['GET'])
-def test():
-    return jsonify({"Message": "Working"})
+
+class User(db.Document):
+    email = db.StringField(required=True, unique=True)
+    password = db.StringField(required=True, min_length=6)
+    created = db.DateTimeField(default=datetime.datetime.now, required=True)
+    updated = db.DateTimeField(default=datetime.datetime.now, required=True)
+
+
+class Profile(db.Document):
+    id_user = db.StringField(required=True)
+    name = db.StringField()
+    surname = db.StringField()
+    phone = db.StringField()
+    created = db.DateTimeField(default=datetime.datetime.now, required=True)
+    updated = db.DateTimeField(default=datetime.datetime.now, required=True)
+
+
+# Register Route
+
+
+@app.route('/register/', methods=["POST"])
+def create_user():
+    try:
+        data = request.get_json()
+
+        hashed_password = generate_password_hash(
+            data['password'])
+        new_user = User(email=data['email'],
+                        password=hashed_password)
+        new_user.save()
+
+        return jsonify({"Message": "New user created!"}), 200
+
+    except Exception as ex:
+        print(ex)
+        return jsonify({"Message": "Unable to create user!"}), 401
 
 
 if __name__ == "__main__":
